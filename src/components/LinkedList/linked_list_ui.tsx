4f43931a32f900a874e727534f60628b3669d5c2
Saadat Ali <saadatali0202@gmail.com>
2022-09-25 12:02:14 +0500

    Rename to add UI to seperate from the class

---
+++ src/components/LinkedList/linked_list_ui.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import styles from "./linked_list.module.css";
+
+const LinkedListUI = () => {
+  return (
+    <div className={styles['container']}>
+      <h1>Linked List</h1>
+    </div>
+  );
+};
+
+export default LinkedListUI;
